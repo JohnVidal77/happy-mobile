a36ea20edc45f7f7fbc69dd86213a6df6d944c64
John Vidal <ba.john.vidal@gmail.com>
2020-10-18 14:13:34 -0300

    NLW finished

--- src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -1,43 +1,122 @@
-import React from "react";
-import { ScrollView, View, Switch, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, View, Switch, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { RectButton } from "react-native-gesture-handler";
+import * as ImagePicker from "expo-image-picker";
 
 import styles from "./styles";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import api from "../../../services/api";
+interface Position {
+  latitude: number;
+  longitude: number;
+}
 
 export default function OrphanageData() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const params = route.params as Position;
+
+  const [name, setName] = useState("");
+  const [about, setAbout] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [opening_hours, setOpeningHours] = useState("");
+  const [open_on_weekends, setopenOnWeekends] = useState(true);
+  const [images, setImages] = useState<string[]>([]);
+
+  async function handleCreateOrphanage() {
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("about", about);
+    data.append("instructions", instructions);
+    data.append("opening_hours", opening_hours);
+    data.append("open_on_weekends", String(open_on_weekends));
+    data.append("latitude", String(params.latitude));
+    data.append("longitude", String(params.longitude));
+
+    images.forEach((image, index) =>
+      data.append("images", { name: `image_${index}.jpg`, type: "image/jpg", uri: image } as any)
+    );
+
+    try {
+      api.post("/orpahange", data);
+
+      navigation.navigate("OrphanagesMap");
+    } catch (err) {
+      alert("Ops! Algo deu errado, tente novamente");
+    }
+  }
+
+  async function handleSelectImages() {
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (status !== "granted") {
+      return alert("Ops! Precisamos de acesso a suas fotos");
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+
+    if (result.cancelled) return;
+
+    const { uri } = result;
+
+    setImages([...images, uri]);
+  }
+
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
       <Text style={styles.title}>Dados</Text>
 
       <Text style={styles.label}>Nome</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
 
       <Text style={styles.label}>Sobre</Text>
-      <TextInput style={[styles.input, { height: 110 }]} multiline />
-
+      <TextInput style={[styles.input, { height: 110 }]} value={about} onChangeText={setAbout} multiline />
+      {/* 
       <Text style={styles.label}>Whatsapp</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} /> */}
 
       <Text style={styles.label}>Fotos</Text>
-      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
+
+      <View style={styles.uploadedImagesContainer}>
+        {images.map((image) => {
+          return <Image key={image} source={{ uri: image }} style={styles.imagePreview} />;
+        })}
+      </View>
+
+      <TouchableOpacity style={styles.imagesInput} onPress={handleSelectImages}>
         <Feather name="plus" size={24} color="#15B6D6" />
       </TouchableOpacity>
 
       <Text style={styles.title}>Visitação</Text>
 
       <Text style={styles.label}>Instruções</Text>
-      <TextInput style={[styles.input, { height: 110 }]} multiline />
+      <TextInput
+        style={[styles.input, { height: 110 }]}
+        value={instructions}
+        onChangeText={setInstructions}
+        multiline
+      />
 
       <Text style={styles.label}>Horario de visitas</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} value={opening_hours} onChangeText={setOpeningHours} />
 
       <View style={styles.switchContainer}>
         <Text style={styles.label}>Atende final de semana?</Text>
-        <Switch thumbColor="#fff" trackColor={{ false: "#ccc", true: "#39CC83" }} />
+        <Switch
+          thumbColor="#fff"
+          value={open_on_weekends}
+          onValueChange={setopenOnWeekends}
+          trackColor={{ false: "#ccc", true: "#39CC83" }}
+        />
       </View>
 
-      <RectButton style={styles.nextButton} onPress={() => {}}>
+      <RectButton style={styles.nextButton} onPress={handleCreateOrphanage}>
         <Text style={styles.nextButtonText}>Cadastrar</Text>
       </RectButton>
     </ScrollView>
--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import styles from "./styles";
 
 import MapMarker from "../../images/map-marker.png";
 import { RectButton } from "react-native-gesture-handler";
+import api from "../../services/api";
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    url: string;
+    id: number;
+  }>;
+}
 
 function OrphanagesMaps() {
   const navigation = useNavigation();
 
-  function handleNavigationToDetails() {
-    navigation.navigate("OrphanageDetails");
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  function handleNavigationToDetails(id: number) {
+    navigation.navigate("OrphanageDetails", { id });
   }
 
   function handleNavigationToCreate() {
-    navigation.navigate("OrphanageData");
+    navigation.navigate("SelectMapPosition");
   }
+
+  useFocusEffect(() => {
+    api.get("/orphanages").then((res) => {
+      setOrphanages(res.data);
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -29,21 +53,24 @@ function OrphanagesMaps() {
         style={styles.mapStyle}
         initialRegion={{ latitude: -23.558356, longitude: -46.650998, latitudeDelta: 0.008, longitudeDelta: 0.008 }}
       >
-        <Marker
-          icon={MapMarker}
-          calloutAnchor={{ x: 2.7, y: 0.9 }}
-          coordinate={{ latitude: -23.558356, longitude: -46.650998 }}
-        >
-          <Callout onPress={handleNavigationToDetails} tooltip>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Minha casa</Text>
-            </View>
-          </Callout>
-        </Marker>
+        {orphanages.map((orphanage) => (
+          <Marker
+            key={orphanage.id}
+            icon={MapMarker}
+            calloutAnchor={{ x: 2.7, y: 0.9 }}
+            coordinate={{ latitude: orphanage.latitude, longitude: orphanage.longitude }}
+          >
+            <Callout onPress={() => handleNavigationToDetails(orphanage.id)} tooltip>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}>{orphanage.name}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
 
       <View style={styles.footer}>
-        <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
+        <Text style={styles.footerText}>{orphanages.length} Orfanatos encontrados</Text>
         <RectButton style={styles.createOrphangeButton} onPress={handleNavigationToCreate}>
           <Feather name="plus" size={20} color="#fff" />
         </RectButton>
